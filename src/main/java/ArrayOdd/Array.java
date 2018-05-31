0e43ee9ee443e6d4ff005655ad972702b9a6cce5
Strohetska <oksistrog@gmail.com>
2018-05-31 20:51:54 +0300

    ArrayOdd

---
+++ src/main/java/ArrayOdd/Array.java
@@ -0,0 +1,15 @@
+package ArrayOdd;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] nums = {1,3,5,7,9,11,13,15,17,19};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(",");
+            }
+        }
+    }
+
+
+}
