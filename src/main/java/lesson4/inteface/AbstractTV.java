15f48f61f13e216abaefc57649f85c9713261164
yurii <smalkoy@gmail.com>
2018-06-10 16:43:11 +0300

    add lesson4 examples

---
+++ src/main/java/lesson4/inteface/AbstractTV.java
@@ -0,0 +1,4 @@
+package lesson4.inteface;
+
+public abstract class AbstractTV implements IChannelChangeable, IVolumeAdjustable, IPauseable {
+}
---
+++ src/main/java/lesson4/inteface/TVThatExtendsAbstractTV.java
@@ -0,0 +1,5 @@
+package lesson4.inteface;
+
+public abstract class TVThatExtendsAbstractTV {
+
+}
