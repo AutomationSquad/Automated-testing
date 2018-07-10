c2128b6d7da80c5e0988d8f2c9ed2df024f980ba
Evgeniy Voloshyn <evgeniy.voloshyn1@gmail.com>
2018-07-10 23:02:48 +0300

    add lesson7

---
+++ src/main/java/lesson7/WebDriverTest.java
@@ -0,0 +1,18 @@
+package lesson7;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class WebDriverTest {
+
+
+    @Test
+    void webDriverTest(){
+    }
+
+}
